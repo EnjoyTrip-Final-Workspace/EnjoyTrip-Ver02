1e303ba1c76e235892cfbd0147321a6fc2328eda
acrow0330@naver.com <eorud2546@nate.com>
2023-05-16 11:42:07 +0900

    first commit

---
+++ EnjoyTrip-back/src/main/java/com/ssafy/vue/model/mapper/AttrMapper.java
@@ -0,0 +1,23 @@
+package com.ssafy.vue.model.mapper;
+
+import java.sql.SQLException;
+
+import java.util.List;
+
+import com.ssafy.vue.model.Attraction;
+import com.ssafy.vue.model.Search;
+import com.ssafy.vue.model.Sido;
+import com.ssafy.vue.model.gugun;
+
+
+public interface AttrMapper {
+
+	List<Attraction> selectAttr(Search search) throws SQLException;
+
+	Attraction selectAttrById(String contentId) throws SQLException;
+
+	List<Sido> selectSido() throws SQLException;
+
+	List<gugun> selectGugun(int code) throws SQLException;
+
+}
---
+++ EnjoyTrip-back/src/main/java/com/ssafy/vue/model/service/AttrService.java
@@ -0,0 +1,22 @@
+package com.ssafy.vue.model.service;
+
+import java.util.List;
+
+import com.ssafy.vue.model.Attraction;
+import com.ssafy.vue.model.Search;
+import com.ssafy.vue.model.Sido;
+import com.ssafy.vue.model.gugun;
+
+
+public interface AttrService {
+	
+	List<Attraction> search(Search search) throws Exception;
+
+	Attraction detail(String contentId) throws Exception;
+
+	List<Sido> sido() throws Exception;
+
+	List<gugun> getGugun(int code) throws Exception;
+
+
+}
